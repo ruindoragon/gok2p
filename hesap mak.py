321b42692be7a903d02396c3b5440f9dd3b3c06a
ruindoragon <ttk117@hotmail.com>
2024-09-23 14:19:38 +0300

    Add files via upload

---
+++ hesap mak.py
@@ -0,0 +1,11 @@
+def calculate(n1, n2):
+    if == '+':
+        result = n1+n2
+    elif == '-':
+        result = n1-n2
+    elif == '*':
+        result = n1*n2
+    elif == '/':
+        result = n1/n2 
+        return result 
+                   
